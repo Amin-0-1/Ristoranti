3fdef020af9a24f84f7259164f758886df586be1
Amin <mahmoudamin373@gmail.com>
2023-11-01 03:02:24 +0200

    feat:(onboarding)
    - add onboarding screen design
    - add onboarding loaderButton
    - add onboarding material designs
    - add UserdefaultManager.swift file

---
+++ Ristoranti/Application/Common/Utilities/UserdefaultManager.swift
@@ -0,0 +1,30 @@
+//
+//  UserdefaultManager.swift
+//  WezCast
+//
+//  Created by Amin on 04/08/2022.
+//
+
+import Foundation
+
+protocol LocalStorageInterface{
+    func getValue(forKey key:UDKeys)->Any?
+    func set(value val:Any,forKey key:UDKeys)
+}
+enum UDKeys:String{
+    case onboarding
+}
+
+class UserdefaultManager: LocalStorageInterface{
+    static let shared = UserdefaultManager()
+    private let userDefaults:UserDefaults = UserDefaults.standard
+    
+    private init(){}
+    func getValue(forKey key:UDKeys)->Any?{
+        let val = userDefaults.value(forKey:key.rawValue)
+        return val
+    }
+    func set(value val:Any,forKey key:UDKeys){
+        userDefaults.set(val, forKey: key.rawValue)
+    }
+}
---
+++ Ristoranti/Scene/Onboarding/View/VC/OnboardingVC.swift
@@ -0,0 +1,92 @@
+//
+//  OnboardingVC.swift
+//  Ristoranti
+//
+//  Created by Amin on 31/10/2023.
+//
+
+import UIKit
+
+class OnboardingVC: UIViewController {
+
+    @IBOutlet private weak var uiCollection: UICollectionView!
+    @IBOutlet private weak var uiStatusImage: UIImageView!
+    @IBOutlet private weak var uiCaption: UILabel!
+    @IBOutlet private weak var uiButton: LoaderButton!
+    private  var currentVisibleIndex = 0
+    var coordinator:OnboardingCoordinator!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureCollection()
+    }
+    
+    private func configureCollection(){
+        let nib = UINib(nibName: OnboardingCell.nibName, bundle: nil)
+        uiCollection.register(nib, forCellWithReuseIdentifier: OnboardingCell.reuseIdentifier)
+        uiCollection.isUserInteractionEnabled = false
+
+        let layout = UICollectionViewCompositionalLayout { section, evnironment in
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: itemSize, subitems: [item])
+            group.interItemSpacing = .fixed(0)
+            let section = NSCollectionLayoutSection(group: group)
+            section.orthogonalScrollingBehavior = .groupPaging
+            return section
+        }
+        
+        uiCollection.collectionViewLayout = layout
+    }
+    
+    @IBAction func nextButton(_ sender: UIButton) {
+        navigate(next: true)
+    }
+    private func navigate(next:Bool){
+        
+        let newValue = next ? currentVisibleIndex + 1 : currentVisibleIndex - 1
+        guard let _ = OnboardingViewDataModel(rawValue: newValue) else {
+            if next{
+                uiButton.isLoading = true
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2){
+                    self.uiButton.isLoading = false
+                    self.coordinator.onFinishedOnboarding()
+                    
+                }
+            }
+            return
+        }
+        uiCollection.scrollToItem(at: .init(item: newValue, section: 0), at: .centeredHorizontally, animated: true)
+    }
+}
+
+extension OnboardingVC:UICollectionViewDelegate,UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: OnboardingCell.reuseIdentifier , for: indexPath) as? OnboardingCell else {fatalError()}
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        self.currentVisibleIndex = indexPath.item
+        if let cell = cell as? OnboardingCell{
+            guard let state = OnboardingViewDataModel(rawValue: currentVisibleIndex) else {
+                if indexPath.item > 0 {
+                    coordinator.onFinishedOnboarding()
+                }
+                return
+            }
+            
+            UIView.transition(with: uiStatusImage, duration: 0.5, options: .transitionCrossDissolve){
+                self.uiStatusImage.image = UIImage(named: state.stepImgage)
+            }
+            
+            UIView.transition(with: uiCaption, duration: 0.5,options: .transitionCrossDissolve) {
+                self.uiCaption.text = state.description
+            }
+            
+            cell.configure(state: state)
+        }
+    }
+    
+}
