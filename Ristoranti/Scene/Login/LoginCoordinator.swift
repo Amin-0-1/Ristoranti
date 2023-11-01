6a613629f5ea79bad63cbd03c90ad275cbd09cdd
Amin <mahmoudamin373@gmail.com>
2023-11-01 15:52:50 +0200

    feat:(onboarding) (#1)
    - add onboarding screen design
    - add onboarding loaderButton
    - add onboarding material designs
    - add UserdefaultManager.swift file

---
+++ Ristoranti/Scene/Login/LoginCoordinator.swift
@@ -0,0 +1,22 @@
+//
+//  LoginCoordinator.swift
+//  Ristoranti
+//
+//  Created by Amin on 31/10/2023.
+//
+
+import UIKit
+
+protocol LoginCoordinatorProtocol:Coordinator{
+    
+}
+struct LoginCoordinator:LoginCoordinatorProtocol{
+    var navigationController: UINavigationController?
+    
+    func start() {
+        let vc = LoginVC()
+        let viewModel = LoginViewModel(coordinator: self)
+        vc.viewModel = viewModel
+        navigationController?.setViewControllers([vc], animated: true)
+    }
+}
