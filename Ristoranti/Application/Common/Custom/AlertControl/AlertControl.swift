05fdde65967f413ae78747cd30aa3169df486973
Amin-0-1 <mahmoudamin373@gmail.com>
2023-11-04 18:39:47 +0200

    fix: (alert)
    - add custom elegant alert view

---
+++ Ristoranti/Application/Common/Custom/AlertControl/AlertControl.swift
@@ -0,0 +1,20 @@
+//
+//  AlertControl.swift
+//  Ristoranti
+//
+//  Created by Amin on 04/11/2023.
+//
+
+import UIKit
+
+class AlertControl: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
